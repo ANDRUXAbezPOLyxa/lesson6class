9bc68086426947dd5ef18bc9f6d00aca336aa47b
AnduxaBezYxa <artiswall@mail>
2022-09-11 22:10:24 +0300

    task 40

---
+++ task40/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа, принимающая на вход 3 числа и поазывающая есть ли треугольни таой длинны ( длина одной стороны не может превышать сумму двух других)
+Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int c = int.Parse(Console.ReadLine());
+if ( (a+b) > c && (b+c)> a && ( a+c) > b)
+{
+ Console.WriteLine("треугольник со сторонами " + a + "," + b + "," + c + " существует");
+}
+else
+ {
+Console.WriteLine("треугольник со сторонами " + a + "," + b + "," + c + " не существует");
+ }
