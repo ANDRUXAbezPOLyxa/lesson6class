2b60239035094a8a32708ced8a1e0da1a2a7acf4
AnduxaBezYxa <artiswall@mail>
2022-09-11 23:04:15 +0300

    task 42

---
+++ task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// введите число, его переведут из десятичной системы в двоичную
+Console.WriteLine("введите число чтобы преобоазить его в двоичную систему");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+string b ="";
+while (n >=1)
+{
+    sum = n % 2;
+    b = sum +b;
+
+n = n/2;
+}
+Console.WriteLine(b);
+
